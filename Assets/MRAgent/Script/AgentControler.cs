e5669d0433976683672e7489efdc0be40ce810f6
decchi <idemasahiro@hotmail.co.jp>
2018-12-25 16:21:52 +0900

    initial

---
+++ Assets/MRAgent/Script/AgentControler.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.SpatialMapping;
+using HoloToolkit.Unity;
+using UnityEngine.AI;
+using System;
+using System.Text;
+using UnityEngine.UI;
+
+
+public class AgentControler : Singleton<AgentControler>, IInputClickHandler
+{
+    float eyeSightangle = 80.0f;
+    public GameObject Agent;
+
+    // Consts
+    const float RayCastLength = 10.0f;
+
+
+    NavMeshAgent agent;
+    Animator animator;
+    // Use this for initialization
+    void Start () {
+
+        Agent.SetActive(false);
+        StartCoroutine(loop());
+        if (Agent.GetComponent<NavMeshAgent>() != null )
+        {
+            agent = Agent.GetComponent<NavMeshAgent>();
+        }
+        animator = Agent.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        animator.SetFloat("Speed", agent.velocity.sqrMagnitude);
+
+    }
+
+
+    //AirTapされたときに呼び出される関数
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Vector3 hitPos, hitNormal;
+                RaycastHit hitInfo;
+        Vector3 uiRayCastOrigin = Camera.main.transform.position;
+        Vector3 uiRayCastDirection = Camera.main.transform.forward;
+        if (Physics.Raycast(uiRayCastOrigin, uiRayCastDirection, out hitInfo, RayCastLength, SpatialMappingManager.Instance.LayerMask))
+        {
+            if (!Agent.activeSelf)
+            {
+                Agent.SetActive(true);
+                
+                hitPos = hitInfo.point;
+                hitNormal = hitInfo.normal;
+                Agent.transform.position = hitPos;
+                Vector3 heading = Agent.transform.position - Camera.main.transform.position;
+                heading.y = 0;
+                Agent.transform.rotation = Quaternion.LookRotation(heading);
+                Agent.transform.rotation = Agent.transform.rotation * Quaternion.Euler(0, 180, 0);
+                ConversationManager.Instance.Initialize();
+            }
+                 
+        }
+
+    }
+
+    private IEnumerator loop()
+    {
+        // ループ
+        while (true)
+        {
+            // 1秒毎にループします
+            yield return new WaitForSeconds(1f);
+
+            if (agent.velocity.sqrMagnitude == 0 && YomeFocusAction.isYomeGazed)
+            {
+                Vector3 heading = Agent.transform.position - Camera.main.transform.position;
+                heading.y = 0;
+                Agent.transform.rotation = Quaternion.LookRotation(heading);
+                Agent.transform.rotation = Agent.transform.rotation * Quaternion.Euler(0, 180, 0);
+            }
+        }
+    }
+
+    public void ComeHere()
+    {
+        agent.destination = Camera.main.transform.TransformPoint(0f,0f,1f);
+    }
+
+
+}
---
+++ Assets/MRAgent/Script/ConversationManager.cs
@@ -0,0 +1,152 @@
+﻿using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+using UnityEngine.Networking;
+using System;
+using System.Text;
+using HoloToolkit.Unity.SpatialMapping;
+
+[RequireComponent(typeof(AgentControler))]
+public class ConversationManager : Singleton<ConversationManager> {
+    [SerializeField]
+    public Text answerTextField;
+    public Text inputTextField;
+    public Image XPic;
+    public Image MicPic;
+    public TextToSpeech textToSpeech;
+    DictationRecognizer m_DictationRecognizer;
+    public GameObject Speeker;
+    private string URL = "https://api.api.ai/v1/query?v=20150910";
+    string str1 = "{\"query\":\"";
+    string str2 = "\",\"lang\":\"en\",\"sessionId\":\"123456789\"}";
+    private string FollowMe_intent = "ComeHere";
+    private const float RayCastLength = 10.0f;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    public void Initialize()
+    {
+        //Step1 音声認識の実装
+
+        XPic.gameObject.SetActive(false);
+    }
+
+    IEnumerator GetReply(string text)
+    {
+
+        var request = new UnityWebRequest(URL, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(str1 + text + str2);
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        //Step2 対話エージェントの設定
+        request.SetRequestHeader("Authorization", "Bearer (set key here)");
+
+
+        yield return request.Send();
+
+        Debug.Log("Status Code: " + request.responseCode);
+        Debug.Log("Result: " + request.downloadHandler.text);
+        ApiaiJson respose = JsonUtility.FromJson<ApiaiJson>(request.downloadHandler.text);
+
+        answerTextField.text = respose.result.fulfillment.speech;
+
+        //Step3 音声合成の実装
+        
+
+        if (respose.result.metadata.intentName.Equals(FollowMe_intent))
+        {
+            YomeControler.Instance.ComeHere();
+        }
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+
+        if (m_DictationRecognizer != null)
+        {
+            if (m_DictationRecognizer.Status == SpeechSystemStatus.Stopped)
+            {
+
+
+                if (YomeFocusAction.isYomeGazed)
+                {
+                    m_DictationRecognizer.Start();
+                    XPic.gameObject.SetActive(false);
+                }
+                else
+                {
+
+                    if (!XPic.IsActive())
+                    {
+                        XPic.gameObject.SetActive(true);
+                    }
+
+
+                }
+
+
+            }
+            else if (m_DictationRecognizer.Status == SpeechSystemStatus.Running)
+            {
+                if (XPic.IsActive())
+                {
+                    XPic.gameObject.SetActive(false);
+                }
+
+            }
+        }
+
+
+
+
+    }
+
+    [Serializable]
+    public class ApiaiJson
+    {
+        public string id;
+        public string timestamp;
+        public string lang;
+        public ApiaiJsonResult result;
+        public string sessionId;
+    }
+
+    [Serializable]
+    public class ApiaiJsonResult
+    {
+        public string source;
+        public string resolvedQuery;
+        public string action;
+        public bool actionIncomplete;
+        public ApiaiJsonFulfillment fulfillment;
+        public ApiaiJsonMetadata metadata;
+        public float score;
+
+    }
+
+    [Serializable]
+    public class ApiaiJsonFulfillment
+    {
+        public string speech;
+    }
+
+    [Serializable]
+    public class ApiaiJsonMetadata
+    {
+        public string intentId;
+        public string webhookUsed;
+        public string webhookForSlotFillingUsed;
+        public string intentName;
+    }
+}
